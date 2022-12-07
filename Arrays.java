ba2b5f48416b14215721a7c35289443b97c01f8a
sourav19us <sourav19us@gmail.com>
2022-12-07 20:21:42 +0530

    Add file

--- Arrays.java
+++ Arrays.java
@@ -41,5 +41,59 @@ public class Arrays {
 
         ary4 = new int[3]; // Now array size is change and new array is created
 
+        // >>>>>>>>>>>> 2D Array <<<<<<<<<<<<<<<
+        int arr3[][]; // int []arr3[]; // int [][]arr3;
+        arr3 = new int[3][3];
+
+        int arr4[][] = { { 1, 5, 2 }, { 14, 9 }, { 4, 9, 3, 89 } };
+        System.out.println(arr4.length);
+        System.out.println(arr4[0].length);
+        for (int i = 0; i < arr4.length; i++) {
+            for (int q = 0; q < arr4[i].length; q++) {
+                System.out.print(arr4[i][q] + " ");
+            }
+            System.out.println();
+        }
+
+        // >>>>>>>>>> 3D Array / Jagged Array <<<<<<<<<<<<<
+
+        int arr5[][][] = new int[][][] { { { 1, 5, 2 }, { 14, 9 }, { 4, 9, 3, 89 } },
+                { { 1, 5, 2 }, { 14, 9 }, { 4, 9, 3, 89 } } };
+
+        int arr6[][][] = new int[2][][];
+        arr6[0] = new int[2][6];
+        arr6[1] = new int[3][5];
+
+        int arr7[][][] = new int[2][][]; // 2 is no of table
+        arr7[0] = new int[3][]; // arr7[a] = new int[b][]; a is table no. ,b is row no.
+        arr7[0][0] = new int[5]; // arr7[a][b] = new int[c]; a is table no. ,b is row no. ,c is
+        // element in rows
+        arr7[0][1] = new int[15];
+        arr7[0][2] = new int[13];
+        arr7[1] = new int[3][];
+        arr7[1][0] = new int[5];
+        arr7[1][1] = new int[15];
+        arr7[1][2] = new int[13];
+
+        for (int i = 0; i < arr5.length; i++) {
+            System.out.println("Table no :-" + (i + 1));
+            for (int q = 0; q < arr5[i].length; q++) {
+                for (int w = 0; w < arr5[i][q].length; w++) {
+                    System.out.print(arr5[i][q][w] + " ");
+                }
+                System.out.println();
+            }
+        }
+        for (int i = 0; i < arr7.length; i++) {
+            System.out.println("Table no :-" + (i + 1));
+            for (int q = 0; q < arr7[i].length; q++) {
+                for (int w = 0; w < arr7[i][q].length; w++) {
+                    arr7[i][q][w] = (int) (Math.random() * 100);
+                    System.out.print(arr7[i][q][w] + " ");
+                }
+                System.out.println();
+            }
+        }
+
     }
 }
--- ExceptionsHandling.java
+++ ExceptionsHandling.java
@@ -1,3 +1,4 @@
+
 public class ExceptionsHandling {
     public static void main(String[] args) {
 
