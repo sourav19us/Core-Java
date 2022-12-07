f6017bb8ff855f4676dcb8d513b0978031632f46
sourav19us <sourav19us@gmail.com>
2022-12-07 20:26:03 +0530

    Add file

--- ExceptionsHandling.java
+++ ExceptionsHandling.java
@@ -1,6 +1,13 @@
 
 public class ExceptionsHandling {
     public static void main(String[] args) {
-
+        int a = 12;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println("Error is :-");
+            System.out.println(e);
+        }
     }
 }
