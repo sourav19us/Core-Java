f4d9db0a48d492316c45ff531ea6929dabaf78c4
sourav19us <sourav19us@gmail.com>
2022-12-21 12:17:06 +0530

    update inheritace code and add same new code

--- Inheritance/InheritanceEx.java
+++ Inheritance/InheritanceEx.java
@@ -1,35 +1,48 @@
-class one{
-    public void say(){
-       System.out.println("Hello class one say()"); 
-    }
-    public static void dis(){
-      System.out.println("Hello class one dis()");
-    }
-    public void get(){
-        say();
-        dis();
-    }
+class one {
+  public void say() {
+    System.out.println("Hello class one say()");
+  }
+
+  public static void dis() {
+    System.out.println("Hello class one dis()");
+  }
+
+  public void get() {
+    say();
+    dis();
+  }
 }
 
-class two extends one{
-    public void say(){
-       System.out.println("Hello class two"); 
-    }
-    public static void dis(){
-      System.out.println("Hello class two dis()");
-    }
-    public void get(){
-        say();
-        dis();
-    }
+class two extends one {
+  public void say() {
+    System.out.println("Hello class two");
+  }
+
+  public static void dis() {
+    System.out.println("Hello class two dis()");
+  }
+
+  public void get() {
+    say();
+    dis();
+  }
 }
 
-public class InheritanceEx
-{
-	public static void main(String[] args) {
-		one ref = new two();
-		ref.say();
-		ref.dis();
-		ref.get();
-	}
+public class InheritanceEx {
+  public static void main(String[] args) {
+    one ref = new two();
+    ref.say();
+    ref.dis();
+    ref.get();
+
+    // we can also call class method by that way
+    new two().get(); // ok class two get method call
+
+    // object type casting
+    two ref1;
+    ref1 = (two) ref; // object to object typecasting
+    ref1.say();
+    ref1.dis();
+    ref1.get();
+  }
 }
