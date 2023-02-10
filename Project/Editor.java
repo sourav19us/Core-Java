ff72ac6b33bcd4e89a1b62d763b48a2e796cc8c3
sourav19us <sourav19us@gmail.com>
2023-02-10 10:44:13 +0530

    add files of Editor Project

---
+++ Project/Editor.java
@@ -0,0 +1,188 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Editor implements ActionListener {
+    Frame f;
+    MenuBar mb;
+    Menu m1, m2, m3;
+    MenuItem nw, opn, sve, sveas, ext, ct, cpy, pst, fd, fdr;
+    static TextArea ta;
+    String spath;
+    int i = 0;
+
+    public Editor() {
+        f = new Frame("Editor");
+        f.setSize(500, 500);
+        f.setVisible(true);
+        ta = new TextArea();
+        mb = new MenuBar();
+        m1 = new Menu("File");
+        m1.addActionListener(this);
+        m2 = new Menu("Edit");
+        m2.addActionListener(this);
+        m3 = new Menu("Others");
+        m3.addActionListener(this);
+        nw = new MenuItem("New");
+        nw.addActionListener(this);
+        opn = new MenuItem("Open");
+        opn.addActionListener(this);
+        sve = new MenuItem("Save");
+        sve.addActionListener(this);
+        sveas = new MenuItem("Save As");
+        sveas.addActionListener(this);
+        ext = new MenuItem("Exit");
+        ext.addActionListener(this);
+        ct = new MenuItem("Cut");
+        ct.addActionListener(this);
+        cpy = new MenuItem("Copy");
+        cpy.addActionListener(this);
+        pst = new MenuItem("Paste");
+        pst.addActionListener(this);
+        fd = new MenuItem("Find");
+        fd.addActionListener(this);
+        fdr = new MenuItem("Find & Replace");
+        fdr.addActionListener(this);
+
+        m2.add(fd);
+        m2.add(fdr);
+        m1.add(nw);
+        m1.add(opn);
+        m1.add(sve);
+        m1.add(sveas);
+        m1.addSeparator();
+        m1.add(ext);
+        mb.add(m1);
+        mb.add(m2);
+        f.add(ta);
+        f.setMenuBar(mb);
+
+    }
+
+    public static TextArea getTextArea() {
+        return ta;
+    }
+
+    public static void setTextArea(String str1) {
+        ta.setText(ta.getText() + str1);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String str = e.getActionCommand();
+        System.out.println(str + " was clicked");
+        if (str.equals("New")) {
+            ta.setText("");
+            i = 0;
+        }
+        // >>>>>>>> Opne <<<<<<<<<<
+        if (str.equals("Open")) {
+            ta.setText("");
+            FileDialog fd = new FileDialog(f, "LOAD File ", FileDialog.LOAD);
+            fd.setVisible(true);
+            String path;
+            path = fd.getDirectory() + fd.getFile();
+            System.out.println(path);
+            File fi = new File(path);
+            FileInputStream fis;
+            BufferedInputStream bis;
+
+            int ch;
+
+            if (fi.exists()) {
+                try {
+                    fis = new FileInputStream(fi);
+                    bis = new BufferedInputStream(fis, 50000);
+
+                    while ((ch = bis.read()) != -1) {
+                        String st = "" + (char) ch;
+                        ta.setText(ta.getText() + st);
+                    }
+
+                    fis.close();
+                } catch (Exception e1) {
+                    System.out.println(e1.getMessage());
+                }
+
+            } else {
+                System.out.println("file not found");
+            }
+            i++;
+        }
+        // >>>>>>>>> Save <<<<<<<<<
+        if (str.equals("Save")) {
+            DataOutputStream dos;
+            FileOutputStream fos;
+            if (i == 0) {
+
+                FileDialog fd = new FileDialog(f, "Save File ", FileDialog.SAVE);
+                fd.setVisible(true);
+
+                spath = fd.getDirectory() + fd.getFile();
+
+                try {
+                    File f = new File(spath);
+                    fos = new FileOutputStream(spath, true);
+                    dos = new DataOutputStream(fos);
+                    if (f.exists()) {
+
+                        String st = ta.getText();
+                        dos.writeChars(st);
+                        dos.close();
+                        i++;
+                    }
+                } catch (Exception e1) {
+                    System.out.println(e1.getMessage());
+                }
+            } else {
+                File f = new File(spath);
+                System.out.println(spath);
+                f.delete();
+                try {
+                    fos = new FileOutputStream(spath, true);
+                    dos = new DataOutputStream(fos);
+                    if (f.exists()) {
+
+                        String st = ta.getText();
+                        dos.writeChars(st);
+                        dos.close();
+                    }
+                } catch (Exception e1) {
+                    System.out.println(e1.getMessage());
+                }
+            }
+        }
+        // >>>>>>>>> Save As <<<<<<<<<
+        if (str.equals("Save As")) {
+            FileDialog fd = new FileDialog(f, "Save File ", FileDialog.SAVE);
+            fd.setVisible(true);
+            String path;
+            path = fd.getDirectory() + fd.getFile();
+            DataOutputStream dos;
+            try {
+                File f = new File(path);
+                FileOutputStream fos = new FileOutputStream(path, true);
+                dos = new DataOutputStream(fos);
+                if (f.exists()) {
+
+                    String st = ta.getText();
+                    dos.writeChars(st);
+                    dos.close();
+                }
+            } catch (Exception e1) {
+                System.out.println(e1.getMessage());
+            }
+        }
+        if (str.equals("Find")) {
+            FFrame ff = new FFrame();
+        }
+        if (str.equals("Find & Replace")) {
+        }
+        if (str.equals("Exit")) {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Editor d = new Editor();
+    }
+}
---
+++ Project/FFrame.java
@@ -0,0 +1,84 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.Vector;
+import java.util.regex.*;
+
+public class FFrame extends WindowAdapter implements ActionListener {
+    Frame f1;
+    TextField tf;
+    Vector<Integer> vec1 = new Vector<Integer>();
+    Vector<Integer> vec2 = new Vector<Integer>();
+    int i = 0;
+
+    public FFrame() {
+        f1 = new Frame();
+        f1 = new Frame("Find");
+        f1.setSize(250, 120);
+        f1.setVisible(true);
+        f1.setLayout(new GridBagLayout());
+        GridBagConstraints gbl = new GridBagConstraints();
+        gbl.gridx = 0;
+        gbl.gridy = 0;
+        gbl.weightx = 1.0;
+        gbl.weighty = 1.0;
+        gbl.anchor = GridBagConstraints.WEST;
+        Label l = new Label("Find");
+        f1.add(l, gbl);
+        tf = new TextField(15);
+        gbl.gridx = 0;
+        gbl.gridy = 1;
+        f1.add(tf, gbl);
+        Button b1 = new Button("FindNext");
+        b1.addActionListener(this);
+        gbl.gridx = 0;
+        gbl.gridy = 2;
+        f1.add(b1, gbl);
+        Button b2 = new Button("Close");
+        b2.addActionListener(this);
+        gbl.gridx = 1;
+        gbl.gridy = 2;
+        f1.add(b2, gbl);
+
+    }
+
+    public void actionPerformed(ActionEvent e3) {
+        String str = e3.getActionCommand();
+        System.out.println(str + " was clicked");
+        if (str.equals("Close")) {
+            // System.exit(1);
+            i = 0;
+            f1.setVisible(false);
+        }
+        if (str.equals("FindNext")) {
+            if (i == 0) {
+
+                TextArea ta1 = Editor.getTextArea();
+                String st = ta1.getText();
+                String st2 = tf.getText();
+                Pattern p = Pattern.compile(st2);
+                Matcher m = p.matcher(st);
+
+                while (m.find()) {
+
+                    System.out.println(m.start() + " " + m.end() + " " + m.group());
+                    vec1.add(m.start());
+                    vec2.add(m.end());
+                }
+            }
+            System.out.println(vec1.size());
+            // if (i < vec1.size()) {
+            // Editor.getTextArea().select(vec1.get(0), vec2.get(0));
+            Editor.getTextArea().select(vec1.get(1), vec2.get(1));
+            Editor.getTextArea().select(vec1.get(2), vec2.get(2));
+            Editor.getTextArea().select(vec1.get(3), vec2.get(3));
+            Editor.getTextArea().select(vec1.get(4), vec2.get(4));
+            Editor.getTextArea().select(vec1.get(5), vec2.get(5));
+            // i++;
+            // System.out.println(i);
+            // }
+
+        }
+    }
+
+}
